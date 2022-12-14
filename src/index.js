c8d5613dbef5c5015700150adebe07cba525f1aa
edalet090 <edaletmollayev090@gmail.com>
2022-12-14 12:44:24 +0400

    comittttt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./state/store";
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+  </BrowserRouter>
+)
